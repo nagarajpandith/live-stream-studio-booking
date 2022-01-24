21066185ecc10ea8647ad73c37d628cc7cbe0024
Dummy Redirect for Hackerearth <nagaraj.pandith2002@gmail.com>
2022-01-24 21:46:20 +0530

    admin-panel

--- studio/admin.py
+++ studio/admin.py
@@ -6,15 +6,15 @@ from django.http import HttpResponse
 # Register your models here.
 class BookingAdmin(admin.ModelAdmin):
     date_hierarchy = 'event_date'
-    list_display = ("first_name", "first_name", "request", "event_date", "email",)
-    list_filter = ("event_date",)
+    list_display = ("first_name", "first_name", "request", "event_date", "event_time","email",)
+    list_filter = ("event_date", "event_time")
     actions=['export_as_csv']
     def export_as_csv(ModelAdmin, request, queryset):
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="bookings.csv"'
         writer = csv.writer(response)
-        writer.writerow(['Event Name', 'Event Date', 'First Name', 'Last Name', 'Email',])
-        bookings = queryset.values_list('request', 'event_date', 'first_name', 'last_name', 'email',)
+        writer.writerow(['Event Name', 'Event Date', 'Event Time','First Name', 'Last Name', 'Email',])
+        bookings = queryset.values_list('request', 'event_date', 'event_time','first_name', 'last_name', 'email',)
         for booking in bookings:
             writer.writerow(booking)
         return response
